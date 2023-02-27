4f8c4021f3373fa46237ea2c55c93037820e3d04
Ekrem Meşe <ekremmese1@gmail.com>
2023-02-27 21:54:21 +0300

    New codes added!

--- src/test/java/com/cydeo/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/LoginStepDefinitions.java
@@ -1,6 +1,7 @@
 package com.cydeo.step_definitions;
 
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
@@ -39,5 +40,9 @@ public class LoginStepDefinitions {
     @And("user enters admin password")
     public void userEntersAdminPassword() {
         System.out.println("user enters admin password");
+    }
+
+    @Given("user is on the library login page")
+    public void userIsOnTheLibraryLoginPage() {
     }
 }
